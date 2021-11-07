857afd3f3c64707283bcccc2cc131fa430eb2371
MartinSilen <iokiokna@gmail.com>
2021-11-07 09:45:24 +0200

    fix

--- src/App.java
+++ src/App.java
@@ -4,25 +4,28 @@ public class App {
     public static void main(String[] args) {
         Scanner com = new Scanner(System.in);
 
-        String result;
+        String result = "";
+        String fromFile;
         while (true) {
             try {
                 System.out.println("Type in the file name:");
                 String command = com.nextLine();
-                result = Reader.read(command);
+                fromFile = Reader.read(command);
                 break;
             } catch (Exception e) {
                 System.out.println("Wrong Filename!");
-                
+
             }
         }
-        System.out.println(result);
-        int counter = 0;
-        for (int k = 0; k < result.length(); k++){
-            if(counter == 2) {System.out.print(result.charAt(k)); counter = 0;}
-            else counter++;
+        System.out.println(fromFile);
+
+        for (int k = 2; k < fromFile.length(); k+=3){
+            result += fromFile.charAt(k);
+
+
 
         }
+        System.out.print(result);
 
 
     }
